eecf86adc6b5dac552302f651ca7a9473ad99b6a
BenvindoDev <allinysilvamachado@gmail.com>
2022-06-15 18:57:09 -0300

    iniciando configuracoes para projeto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'soul-digital-ea164',
+    appId: '1:533175354698:web:11bc23fcc83784aa93edb3',
+    storageBucket: 'soul-digital-ea164.appspot.com',
+    apiKey: 'AIzaSyCXOb41huxRMlYTluY2-kcEdT5nUiFDGLc',
+    authDomain: 'soul-digital-ea164.firebaseapp.com',
+    messagingSenderId: '533175354698',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'soul-digital-ea164',
+    appId: '1:533175354698:web:11bc23fcc83784aa93edb3',
+    storageBucket: 'soul-digital-ea164.appspot.com',
+    apiKey: 'AIzaSyCXOb41huxRMlYTluY2-kcEdT5nUiFDGLc',
+    authDomain: 'soul-digital-ea164.firebaseapp.com',
+    messagingSenderId: '533175354698',
+  },
   production: false
 };
 
